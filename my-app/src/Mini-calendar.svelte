c16d2da366ed0eec45862361d68b2e77908a695a
Maximeqbp <106659771+Maximeqbp@users.noreply.github.com>
2022-12-15 13:21:10 +0100

    agenda

---
+++ my-app/src/Mini-calendar.svelte
@@ -0,0 +1,11 @@
+<div class="mini-calendar" />
+
+<style>
+  .mini-calendar {
+    border: 1px solid white;
+    width: 200px;
+    height: 200px;
+    top: 50px;
+    position: relative;
+  }
+</style>
